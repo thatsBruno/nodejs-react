6deb0e4698365f2b3d1f6a5ef1d276d1609c38b5
Bruno Ribeiro <brunoandreribeiro18@gmail.com>
2024-02-13 22:30:57 +0000

    mongodb setup

--- server/server.js
+++ server/server.js
@@ -1,20 +1,18 @@
-const express = require('express')
-const app = express()
-const mongoose = require("mongoose")
-require("dotenv").config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 5001;
 
-
-const userRoute = require("./routes/users-route")
-
-app.use("/users", userRoute)
-
-mongoose.connect(
-    process.env.MONGODB_URL, 
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
-);
-
-
-app.listen(5001, () => {console.log("Server started on http://localhost:5001")})
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/record"));
+// get driver connection
+const dbo = require("./database/connection");
+app.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+  });
+  console.log(`Server is running on port: http://localhost:${port}`);
+});
