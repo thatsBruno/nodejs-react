abaf019efdac7fa0a8317e6419c31a50b91a37cd
Bruno Ribeiro <brunoandreribeiro18@gmail.com>
2024-02-13 21:50:50 +0000

    added mongodb

--- server/server.js
+++ server/server.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const app = express()
+const mongoose = require("mongoose")
+require("dotenv").config()
+
 
 const userRoute = require("./routes/users-route")
 
 app.use("/users", userRoute)
+
+mongoose.connect(
+    process.env.MONGODB_URL, 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
 
 
 app.listen(5001, () => {console.log("Server started on http://localhost:5001")})
