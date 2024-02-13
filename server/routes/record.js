1d6ba2ae271b6d4ffeea670f5869a7a356c24758
Bruno Ribeiro <brunoandreribeiro18@gmail.com>
2024-02-13 22:33:21 +0000

    cleanup

--- server/routes/record.js
+++ server/routes/record.js
@@ -1,19 +1,12 @@
 const express = require("express");
-
-// recordRoutes is an instance of the express router.
-// We use it to define our routes.
-// The router will be added as a middleware and will take control of requests starting with path /record.
 const recordRoutes = express.Router();
-
-// This will help us connect to the database
 const dbo = require("../database/connection");
 
-// This help convert the id from string to ObjectId for the _id.
 const ObjectId = require("mongodb").ObjectId;
 
 // This section will help you get a list of all the records.
 recordRoutes.route("/record")
-    .get(function (req, res) {
+    .get((req, res) => {
     let db_connect = dbo.getDb("employees");
     db_connect
         .collection("records")
@@ -26,10 +19,10 @@ recordRoutes.route("/record")
 
 // This section will help you get a single record by id
 recordRoutes.route("/record/:id")
-    .get(function (req, res) {
+    .get((req, res) => {
     let db_connect = dbo.getDb();
     let myquery = { _id: ObjectId(req.params.id) };
-    db_connect.collection("records").findOne(myquery, function (err, result) {
+    db_connect.collection("records").findOne(myquery, (err, result) => {
         if (err) throw err;
         res.json(result);
     });
@@ -37,14 +30,14 @@ recordRoutes.route("/record/:id")
 
 // This section will help you create a new record.
 recordRoutes.route("/record/add")
-    .post(function (req, response) {
+    .post((req, response) => {
     let db_connect = dbo.getDb();
     let myobj = {
         name: req.body.name,
         position: req.body.position,
         level: req.body.level,
     };
-    db_connect.collection("records").insertOne(myobj, function (err, res) {
+    db_connect.collection("records").insertOne(myobj, (err, res) => {
         if (err) throw err;
         response.json(res);
     });
@@ -52,7 +45,7 @@ recordRoutes.route("/record/add")
 
 // This section will help you update a record by id.
 recordRoutes.route("/update/:id")
-    .post(function (req, response) {
+    .post((req, response) => {
     let db_connect = dbo.getDb();
     let myquery = { _id: ObjectId(req.params.id) };
     let newvalues = {
@@ -64,7 +57,7 @@ recordRoutes.route("/update/:id")
   };
   db_connect
     .collection("records")
-    .updateOne(myquery, newvalues, function (err, res) {
+    .updateOne(myquery, newvalues, (err, res) => {
       if (err) throw err;
       console.log("1 document updated");
       response.json(res);
@@ -76,7 +69,7 @@ recordRoutes.route("/:id")
     .delete((req, response) => {
     let db_connect = dbo.getDb();
     let myquery = { _id: ObjectId(req.params.id) };
-    db_connect.collection("records").deleteOne(myquery, function (err, obj) {
+    db_connect.collection("records").deleteOne(myquery, (err, obj) => {
      if (err) throw err;
         console.log("1 document deleted");
      response.json(obj);
